4bada2772e0d5537471c66b486c20540265ab271
Dennis Kerry <dmkerry7427@eagle.fgcu.edu>
2024-09-30 16:39:09 -0400

    fix: change int testing values to be a bit easier on the eyes

--- src/list_test.cpp
+++ src/list_test.cpp
@@ -22,18 +22,18 @@ int main()
     }
     for (int i = 0; i < 6; i++)
     {
-        std::cout << list.read(i) << ' ';
+        std::cout << list.read(i) << ' ';  // should look like 0 1 2 3 4 5
     }
     std::cout << std::endl;
-    list.insert(9283749, 3);  // add a random number to the mix
+    list.insert(49, 3);  // add a random number to the mix
     list.insert(23);  // insert 23 at end of list
     list.remove(6);  // get rid of the last node
     list.modify(0, 56); // change the head to hold 56
 
     for (int i = 0; i < 7; i++)
     {
-        // should look like 56 1 2 9283749 3 4 23
-        std::cout << list.read(i) << ' ';
+        // should look like 56 1 2 49 3 4 23
+        std::cout << list.read(i) << ' '; // and it does! yay!
     }
     return 0;
 }
